0420b902032708f99b741a09e870955815692ab2
NazranKhondokar <nazran91@gmail.com>
2019-06-18 14:11:06 +0600

    Auto Validation

---
+++ authlib/src/main/java/com/rokomari/authlib/GenericTextWatcher.java
@@ -0,0 +1,25 @@
+package com.rokomari.authlib;
+
+import android.text.Editable;
+import android.text.TextWatcher;
+
+import com.rokomari.authlib.formvalidator.widget.FormEditText;
+
+/**
+ * Created by user on 6/18/2019.
+ */
+
+public class GenericTextWatcher implements TextWatcher {
+
+    private FormEditText mFormEditText;
+    public GenericTextWatcher(FormEditText mFormEditText) {
+        this.mFormEditText = mFormEditText;
+    }
+
+    public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {}
+    public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {}
+
+    public void afterTextChanged(Editable editable) {
+        mFormEditText.validate();
+    }
+}
