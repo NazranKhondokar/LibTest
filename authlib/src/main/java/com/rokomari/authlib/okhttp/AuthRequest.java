55cc401de4cfa2ac38038422a43b73a461e55ee4
NazranKhondokar <nazran91@gmail.com>
2019-06-26 17:33:16 +0600

    add AuthRequest

---
+++ authlib/src/main/java/com/rokomari/authlib/okhttp/AuthRequest.java
@@ -0,0 +1,84 @@
+package com.rokomari.authlib.okhttp;
+
+import com.google.gson.Gson;
+import com.rokomari.authlib.okhttp.callback.StringCallback;
+
+import java.util.Map;
+
+/**
+ * Created by user on 6/23/2019.
+ */
+
+public class AuthRequest<T> {
+
+    private String mBaseUrl;
+    private String endPoint;
+    private Map<String, String> headers;
+    private Map<String, String> params;
+    private T obj;
+    private StringCallback stringCallback;
+
+    public AuthRequest(String mBaseUrl, String endPoint, Map<String, String> headers, T obj, StringCallback stringCallback) {
+        this.mBaseUrl = mBaseUrl;
+        this.endPoint = endPoint;
+        this.headers = headers;
+        this.obj = obj;
+        this.stringCallback = stringCallback;
+    }
+
+    public AuthRequest(String mBaseUrl, String endPoint, Map<String, String> headers, StringCallback stringCallback) {
+        this.mBaseUrl = mBaseUrl;
+        this.endPoint = endPoint;
+        this.headers = headers;
+    }
+
+    public AuthRequest(String mBaseUrl, String endPoint, Map<String, String> headers, Map<String, String> params) {
+        this.mBaseUrl = mBaseUrl;
+        this.endPoint = endPoint;
+        this.headers = headers;
+        this.params = params;
+    }
+
+    public void callPOST() {
+        String url = mBaseUrl + endPoint;
+        OkHttpUtils
+                .postString()
+                .url(url)
+                .headers(headers)
+                .content(new Gson().toJson(obj))
+                .build()
+                .execute(stringCallback);
+    }
+
+    public void callGET() {
+        String url = mBaseUrl + endPoint;
+        OkHttpUtils
+                .get()
+                .url(url)
+                .headers(headers)
+                .build()
+                .execute(stringCallback);
+    }
+
+    public void callPOSTWithParams() {
+        String url = mBaseUrl + endPoint;
+        OkHttpUtils
+                .post()
+                .url(url)
+                .headers(headers)
+                .params(params)
+                .build()
+                .execute(stringCallback);
+    }
+
+    public void callGETWithParams() {
+        String url = mBaseUrl + endPoint;
+        OkHttpUtils
+                .get()
+                .url(url)
+                .headers(headers)
+                .params(params)
+                .build()
+                .execute(stringCallback);
+    }
+}
